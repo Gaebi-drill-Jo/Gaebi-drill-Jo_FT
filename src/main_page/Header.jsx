d6108e0662a3cea50872020d5d1c74b3082f9f14
jeajune0221 <jeajune11117@gmail.com>
2025-11-25 22:27:13 +0900

    수정

--- src/main_page/Header.jsx
+++ src/main_page/Header.jsx
@@ -1,48 +1,30 @@
-// Original code preserved for reference:
-// import React from "react";
-// import "./Head.css"
-// import logo from "../images/logo.svg";
-// function Header(){
-//
-//     return (
-//         <div className = "head container">
-//             <img id = "logo1" src={logo} alt="로고" />
-//             <button id = "alertt">알림설정하기</button>
-//             <button id = "peo">만든 사람들</button>
-//             <button id = "grap">그래프</button>
-//             <button id = "chdata">데이터값 조회하기</button>
-//             <button id ="log,logbtn">Sign up</button>
-//         </div>
-//     )
-// }
-//
-//
-//
-//
-// export default Header
-
 import React from 'react';
 import './Head.css';
 import logo from '../images/logo1.png';
 
-const navItems = ['알림 설정하기', '그래프', '데이터 값 조회하기', 'AI피드백'];
-
 function Header() {
   return (
     <header className="head">
-      <div className="head-logo">
+      <div className="logo111">
         <img src={logo} alt="AIRZY 로고" />
       </div>
+      <div className="head_menu" role="네비게이션" aria-label="주요 메뉴">
+  <button className="menu_le" type="button" onClick={() => window.location.href = '/notification'}>
+    알림 설정하기
+  </button>
+  <button className="menu_le" type="button" onClick={() => window.location.href = '/graph'}>
+    그래프
+  </button>
+  <button className="menu_le" type="button" onClick={() => window.location.href = '/data'}>
+    데이터 값 조회하기
+  </button>
+  <button className="menu_le" type="button" onClick={() => window.location.href = '/feedback'}>
+    AI피드백
+  </button>
+</div>
 
-      <nav className="head-nav" aria-label="주요 메뉴">
-        {navItems.map((item) => (
-          <a href="#" key={item} className="nav-link">
-            {item}
-          </a>
-        ))}
-      </nav>
 
-      <button className="head-signup" type="button">
+      <button className="logi" type="button">
         Sign up
       </button>
     </header>
--- src/main_page/Hero_page.jsx
+++ src/main_page/Hero_page.jsx
@@ -9,11 +9,13 @@ function HeroPage() {
       <Header />
       <div className='middle'>
         <span className = "middle_Title">SMART AIR<br/>ANALYZE</span>
-        <span className = "middle_text">We spent three months researching and<br/> developing an atmospheric analysis system,<br/> combining hardware and the web.</span>
+        <div>
+          <span className = "middle_text">We spent three months researching and<br/> developing an atmospheric analysis system,<br/> combining hardware and the web.</span>
+          <button className='get_start'>get_started</button>
+          <button classNaem = 'view_demo'>view_demo</button>
+        </div>
       </div>
-      
         <div className='Hero_content'>
-
         </div>
       <Footer />
       </div>
